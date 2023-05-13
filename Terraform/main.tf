ec9bec5deb44fd7a8c554be8d516ecec0436bdd8
roshan-parabol <roshan@parabol.co>
2023-05-13 18:32:42 -0400

    Created service account for VM management

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -75,7 +75,7 @@ resource "google_storage_bucket" "rethinkdb_backup_bucket" {
   uniform_bucket_level_access = true
 }
 
-resource "google_service_account" "rethinkdb_service_account" {
+resource "google_service_account" "rethinkdb_admin_service_account" {
   account_id   = "rethinkdb-admin"
   display_name = "RethinkDB Storage Admin Service Account"
   project      = var.project_id
@@ -86,6 +86,18 @@ resource "google_storage_bucket_iam_binding" "rethinkdb_bucket_iam_binding" {
   role   = "roles/storage.objectAdmin"
 
   members = [
-    "serviceAccount:${google_service_account.rethinkdb_service_account.email}",
+    "serviceAccount:${google_service_account.rethinkdb_admin_service_account.email}",
   ]
+}
+
+resource "google_service_account" "rethink_vm_service_account" {
+  account_id   = "rethinkdb-vm-admin"
+  display_name = "RethinkDB VM Service Account"
+  project      = var.project_id
+}
+
+resource "google_project_iam_member" "compute_admin" {
+  project = var.project_id
+  role    = "roles/compute.admin"
+  member  = "serviceAccount:${google_service_account.rethink_vm_service_account.email}"
 }
