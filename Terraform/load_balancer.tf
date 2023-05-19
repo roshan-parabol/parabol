912b00f8221f6e9b87eb7af901aeacc27082ed9b
roshan-parabol <roshan@parabol.co>
2023-05-18 22:08:12 -0400

    Remove ssl from load balancer

--- Terraform/load_balancer.tf
+++ Terraform/load_balancer.tf
@@ -7,8 +7,8 @@ resource "google_compute_health_check" "rethinkdb_health_check" {
   name               = "rethinkdb-lb-health-check"
   check_interval_sec = 10
   healthy_threshold  = 2
-  https_health_check {
-    port               = 443
+  http_health_check {
+    port               = 8080
     port_specification = "USE_FIXED_PORT"
     proxy_header       = "NONE"
     request_path       = "/"
@@ -32,45 +32,16 @@ resource "google_compute_backend_service" "rethinkdb_backend_service" {
 resource "google_compute_url_map" "rethinkdb_compute_url_map" {
   name            = "rethinkdb-map-http"
   default_service = google_compute_backend_service.rethinkdb_backend_service.id
-
-  host_rule {
-    hosts        = [google_compute_global_address.rethinkdb_lb_ipv4_1.address]
-    path_matcher = "all-paths"
-  }
-
-  path_matcher {
-    name            = "all-paths"
-    default_service = google_compute_backend_service.rethinkdb_backend_service.id
-  }
-
 }
 
-resource "google_compute_target_https_proxy" "rethinkdb_https_lb_proxy" {
-  provider = google-beta
+resource "google_compute_target_http_proxy" "rethinkdb_http_lb_proxy" {
   name    = "rethinkdb-https-lb-proxy"
-  project  = var.project_id
   url_map = google_compute_url_map.rethinkdb_compute_url_map.id
-  ssl_certificates = [
-    google_compute_managed_ssl_certificate.rethinkdb_ssl_certificate.name
-  ]
-  depends_on = [
-    google_compute_managed_ssl_certificate.rethinkdb_ssl_certificate
-  ]
 }
 
 resource "google_compute_global_forwarding_rule" "rethinkdb_forwarding_rule" {
   name       = "rethinkdb-forwarding-rule"
-  target     = google_compute_target_https_proxy.rethinkdb_https_lb_proxy.id
-  port_range = "443"
+  target     = google_compute_target_http_proxy.rethinkdb_http_lb_proxy.id
+  port_range = "8080"
   ip_address = google_compute_global_address.rethinkdb_lb_ipv4_1.id
-}
-
-resource "google_compute_managed_ssl_certificate" "rethinkdb_ssl_certificate" {
-  provider = google-beta
-  name     = "rethinkdb-ssl-certificate"
-  project  = var.project_id
-
-  managed {
-    domains = ["parabol.co"]
-  }
 }
